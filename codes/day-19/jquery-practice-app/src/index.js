6cc86a1906ae00544fa1db4922af769265c1c7b5
Joydip <joydip.mondal@gmail.com>
2022-11-07 12:33:05 +0530

    day-19 jQuery uploaded

---
+++ codes/day-19/jquery-practice-app/src/index.js
@@ -0,0 +1,54 @@
+const items = ['anil', 'sunil', 'mahesh']
+
+/*
+function loadListItemsUsingJS() {
+    const listElement = document.getElementById('nameList')
+
+    items.forEach(
+        function (name) {
+            const listItem = document.createElement('li')
+            listItem.innerText = name
+            listElement.appendChild(listItem)
+        }
+    )
+}
+window.addEventListener('DOMContentLoaded', loadListItemsUsingJS)
+*/
+function loadListItemsUsingjquery() {
+    items.forEach(
+        function (name) {
+
+            // $('#nameList').append(`<li>${name}</li>`)
+            // const listItem = $('<li/>', { text: name })
+            // listItem.appendTo('#nameList')
+            $('<li/>', { text: name }).appendTo('#nameList')
+        }
+    )
+}
+
+function loadOptionsUisngJQuery() {
+    //code
+    let index = 0
+    items.forEach(
+        (name) => {
+
+            const option = $(
+                '<option/>',
+                {
+                    text: name, value: index,
+                }
+            )
+            option.appendTo('#ddlNames')
+            index++
+        }
+    )
+
+    $('#ddlNames').on('change', (event) => {
+        const selectedOption = event.target.children[event.target.selectedIndex]
+        window.alert(`${selectedOption.text}, ${selectedOption.value}`)
+    })
+}
+$(function () {
+    loadListItemsUsingjquery()
+    loadOptionsUisngJQuery()
+})
